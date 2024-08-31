9f354e8538739c9c883c89814d7153d8dbc2fd67
Mutsuha Asada <me@momee.mt>
2024-08-31 12:45:54 +0900

    add: oss section component

---
+++ src/Components/OSS/Index.purs
@@ -0,0 +1,30 @@
+module OSS where
+
+import Prelude
+
+import ExternalLink (external_link)
+import Jelly.Component (Component, text)
+import Jelly.Element as JE
+import Jelly.Prop ((:=))
+
+oss_li :: forall m. String -> Component m -> Component m
+oss_li name comp = do
+  JE.li ["class" := "mt-1"] do
+    JE.details' do
+      JE.summary ["class" := "list-none"] do
+        text name
+      JE.ul ["class" := "list-disc ml-4"] do
+        comp
+
+oss :: forall m. Component m
+oss = do
+  JE.h2 ["class" := "text-2xl mt-4"] do
+    text "OSS活動"
+  JE.ul ["class" := "list-disc ml-4 mt-2"] do
+    oss_li "NixOS/nixpkgs (2024/05 - 現在)" do
+      JE.li' do
+        external_link "https://github.com/NixOS/nixpkgs/pulls?q=author%3Amomeemt+" do
+          text "Pull Requests (merged: 3, open: 1)"
+      JE.li' do
+        external_link "https://github.com/NixOS/nixpkgs/pulls?q=reviewed-by%3Amomeemt+-author%3Amomeemt" do
+          text "Reviewing (closed: 19, open: 2)"
